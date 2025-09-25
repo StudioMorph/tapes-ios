aef0770e94dded32b81d14464e0ca622bb8a663e
Jose Santos <hi@jose-santos.com>
2025-09-25 18:39:15 +0100

    Initial commit

---
+++ Tapes/TapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapesApp.swift
+//  Tapes
+//
+//  Created by Jose Santos on 25/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapesTests/TapesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapesTests.swift
+//  TapesTests
+//
+//  Created by Jose Santos on 25/09/2025.
+//
+
+import Testing
+@testable import Tapes
+
+struct TapesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
