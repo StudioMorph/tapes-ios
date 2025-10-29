e9f9d956453e444e1c95bc1c0f5d461033a74fbb
Jose Santos <hi@jose-santos.com>
2025-10-29 18:03:21 +0000

    Set TapePlayerView background to black for consistent video viewing experience
    - Changed from adaptive primaryBackground to solid black
    - Provides consistent background in both light and dark modes
    - Temporarily masks video positioning issues

--- Tapes/Views/Player/TapePlayerView.swift
+++ Tapes/Views/Player/TapePlayerView.swift
@@ -37,7 +37,7 @@ struct TapePlayerView: View {
 
     var body: some View {
         ZStack {
-            Tokens.Colors.primaryBackground.ignoresSafeArea()
+            Color.black.ignoresSafeArea()
 
             if let player {
                 VideoPlayer(player: player)
